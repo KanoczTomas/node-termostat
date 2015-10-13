fc085637ae437e76f94beb24be61acfd2d65db61
Tomas Kanocz <tomas.kanocz@cnl.sk>
2015-10-13 09:37:03 +0000

    restarted tests to do it the proper way

--- tests/js/objects/termostat.js
+++ tests/js/objects/termostat.js
@@ -2,17 +2,66 @@ var Termostat = require('../../../js/objects/termostat');
 var should = require('should');
 
 termostat = new Termostat();
-describe('termostat tests', function(){
-  describe('getTemperature', function(){
-    it('should return temperature and be a Number', function(){
-      termostat.temperature = 44;
-      termostat.getTemperature().should.be.exactly(44).and.be.a.Number();
+termostat.auto = false;
+termostat.temperature = 0.0;
+termostat.termostat = 23.0;//termperature to guard for the termostat
+termostat.hysteresis = 1.0;
+termostat.humidity = 100.0;
+termostat.manualSwitch= false;//used to switch pin2 manualy
+termostat.termostatSwitch = false; //used switch pin 1 manualy/automatically
+termostat.manualSwitchPin = 13;//pin used in physical order 
+termostat.manualSwitchPinLock = false;//if we are doing an operation with the pin, we need to lock it
+termostat.termostatSwitchPin = 11; //pin used in physical order 
+termostat.termostatSwitchPinLock = false; //if we are doing an operation with the pin, we need to lock it
+termostat.flipable = 0; //helper variable for auto mode -1 below treshold and hystereses, 0 between, 1 above
 
-      termostat.temperature = 54;
-      termostat.getTemperature().should.be.exactly(54).and.be.a.Number();
-
-      termostat.temperature = 4;
-      termostat.getTemperature().should.be.exactly(4).and.be.a.Number();
-    });
+describe('getMethods', function(){
+  describe('getMode()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getTemperature()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getTermostat()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getHysteresis()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getHumidity()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getManualSwitch()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getTermostatSwitch()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getManualSwitchPin()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getManualSwitchPinLock()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getTermostatSwitchPin()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getTermostatswitchpinlock()',function(){
+      it('should return value of ', function(){
+      });
+  });
+  describe('getFlipable()',function(){
+      it('should return value of ', function(){
+      });
   });
 });
