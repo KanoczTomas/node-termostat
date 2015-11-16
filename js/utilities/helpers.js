8b5847b630227e9cdf98a35679429645778cf755
Tomas Kanocz <tomas.kanocz@cnl.sk>
2015-11-16 20:52:24 +0000

    mingodbWriteHelper test promise issue

--- js/utilities/helpers.js
+++ js/utilities/helpers.js
@@ -1,4 +1,5 @@
 var TermostatModel = require('../models/termostat.js');
+var Promise = require('bluebird');
 
 module.exports = {
   variableChangeHelper: function variableChangeHelper(var1, var2){//return true if variable changed, else false
@@ -6,11 +7,17 @@ module.exports = {
     return true;
   },
   mongodbWriteHelper: function mongodbWriteHelper(object, varToSave){
-    TermostatModel.findById(object._id, function (err, doc){
-      doc[varToSave] = object[varToSave];
-      doc.save(function (err){
-        if(err) throw err;
-      });
+    return new Promise(function(resolve, reject){
+      if (object._id === undefined) reject(Error('No _id property of object'));
+      else{
+        TermostatModel.findById(object._id, function (err, doc){
+          doc[varToSave] = object[varToSave];
+          doc.save(function (err){
+            if(err) reject(err);
+            else resolve();
+          });
+        });
+      }
     });
   }
 }
--- tests/js/utilities/helpers.js
+++ tests/js/utilities/helpers.js
@@ -1,5 +1,11 @@
 var helpers = require('../../../js/utilities/helpers');
+var TermostatModel = require('../../../js/models/termostat');
 var should = require('should');
+should.prototype = new require('should-promised');
+var mongoose = require('mongoose');
+var Termostat = require('../../../js/objects/termostat');
+
+termostat = new Termostat();
 
 describe('Utility functions:', function(){
 
@@ -12,4 +18,53 @@ describe('Utility functions:', function(){
     });
   });
 
+  describe('#mongodbWriteHelper()', function (){
+    before(function(){
+      mongoose.connect('mongodb://localhost/test-termostat');
+    });
+    it('should be rejected and return error when object._id is not found', function(done){
+      helpers.mongodbWriteHelper({test: 'jahoda'}, 'test').should.be.rejected()
+      .then(function(err){
+        should.exist(err);
+	err.should.be.an.Error();
+	done();
+      })
+      .done(null, done);
+    });
+    it('should write the db without errors', function(done){
+      toWrite = new TermostatModel({
+        auto: true
+      });
+      toWrite.save(function(err){
+        should.not.exist(err);
+	done();
+      });
+    });
+    it('should write the database correctly', function(done){
+      TermostatModel.find({}, function(err, out){
+        should.not.exist(err);
+	out.length.should.be.equal(1);
+	termostat._id = out._id;
+	out.auto.should.be.true();
+	done();
+      });
+    });
+    it('should write always to the same database', function(done){
+      termostat.auto = false;
+      helpers.mongodbWriteHelper(termostat, 'auto').should.be.fulfilled()
+      .then(function(){
+        TermostatModel.find({}, function(err, out){
+          out.length.should.be.equal(1);
+          out.auto.should.be.false();
+          done();
+        })
+      })
+      .done(done);
+      after(function(){
+        console.log('todo');
+        //TermostatModel.remove();
+        //mongoose.connection.db.dropDatabase();
+      });
+    });
+  });
 });
